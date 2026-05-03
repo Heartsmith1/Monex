ad53f9522ce82e97739b8f1ff986778bd15f9e8a
Allen ibañez <all.ibanez@duocuc.cl>
2026-05-03 19:57:19 -0400

    avance esqueleto pagina home

--- Monex/frontend/src/pages/home/Home.jsx
+++ Monex/frontend/src/pages/home/Home.jsx
@@ -9,13 +9,29 @@ export function Home() {
             <div className="contenido_Home">
                 <Navbar />
                 <h1>Bienvenido Usuario</h1>
-                
-                <div className="box_grafico_lineal_home">
-                    <h1>Total Gastos del Mes</h1>
 
+                <div className="layout_home">
 
+                    {/* IZQUIERDA */}
+                    <div className="col_izquierda">
+                        <div className="box_grafico_lineal_home">
+                            <h1>Total Gastos del Mes</h1>
+                        </div>
+
+                        <div className="contendor_ultimos_gastos">
+                            <h1>Ultimos Gastos</h1>
+                        </div>
+                    </div>
+
+                    {/* DERECHA */}
+                    <div className="col_derecha">
+                        <div className="box_grafico_torta_home">
+                            <h2>Gastos por categoria</h2>
+                        </div>
+                    </div>
 
                 </div>
+
             </div>
         </div>
     );
