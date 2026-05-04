9adcb4f7826ec9a1e6f8dd8d292a4e24d2c08ca3
Allen ibañez <all.ibanez@duocuc.cl>
2026-05-03 22:33:09 -0400

    agrandar contenedores home

--- Monex/frontend/src/pages/home/Home.jsx
+++ Monex/frontend/src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import { SideBar } from "../../components/SideBar/SideBar";
 import { Navbar } from "../../components/Navbar/Navbar";
 
 export function Home() {
+
+
     return (
         <div className="contenedor_Home">
             <SideBar />
