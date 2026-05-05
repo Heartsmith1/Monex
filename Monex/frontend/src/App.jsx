3ad595a938f94e37e0b50bbeeaf2ec0a37e25e3b
Allen ibañez <all.ibanez@duocuc.cl>
2026-05-04 21:50:48 -0400

    cambio de rutas

--- Monex/frontend/src/App.jsx
+++ Monex/frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css'
-import './css/pages/login.css'
-import './css/pages/register.css'
-import './css/components/sideBar.css'
-import './css/components/navbar.css'
-import './css/pages/home.css'
-import './css/pages/categorias.css'
-import './css/pages/expenses.css'
-import { Categorias } from './pages/categorias/Categorias'
+import './App.css';
+import './css/pages/login.css';
+import './css/pages/register.css';
+import './css/components/sideBar.css';
+import './css/components/navbar.css';
+import './css/pages/home.css';
+import './css/pages/categories.css';
+
+import { Categorias } from './pages/categories/Categories';
 import { Login } from './pages/Login/Login';
 import { Home } from './pages/home/Home';
 import { SideBar } from './components/SideBar/SideBar';
