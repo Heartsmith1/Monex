86ff7fcf56c9d3eb6013071b1c886159abc71556
Carlos López <carloscristianandresl@gmail.com>
2026-05-04 21:43:12 -0400

    Correcion de nombres e imports de clases

--- Monex/frontend/src/App.jsx
+++ Monex/frontend/src/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css'
-import './css/pages/login.css'
-import './css/pages/register.css'
-import './css/components/sideBar.css'
-import './css/components/navbar.css'
-import './css/pages/home.css'
-import './css/pages/categorias.css'
-import { Categorias } from './pages/categorias/Categorias'
+
+import './App.css';
+import './css/pages/login.css';
+import './css/pages/register.css';
+import './css/components/sideBar.css';
+import './css/components/navbar.css';
+import './css/pages/home.css';
+import './css/pages/categories.css';
+
+import { Categorias } from './pages/categories/Categories';
 import { Login } from './pages/Login/Login';
-import {Home} from './pages/home/Home';
+import { Home } from './pages/home/Home';
 import { SideBar } from './components/SideBar/SideBar';
-import { Register } from './pages/Registro/Register'
+import { Register } from './pages/Registro/Register';
 
 function App() {
 
